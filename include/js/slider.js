b57c2eaa17d4e01e885d6746721881b9e73afa81
Somi <somy.taheri@gmail.com>
2014-12-01 20:51:02 +1100

    footer done

--- include/js/slider.js
+++ include/js/slider.js
@@ -1,3 +1,13 @@
 /**
  * Created by somi on 1/12/14.
  */
+
+jQuery(document).ready(function ($) {
+    var options = {
+        $AutoPlay: true,
+        $AutoPlayInterval: 3000,
+        $SlideDuration: 1000,
+    };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+});
