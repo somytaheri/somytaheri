fdd3d07a4d05e987bb241a2f0bf781139a711205
Somi <somy.taheri@gmail.com>
2014-12-03 15:49:58 +1100

    about done

--- public/assets/js/somytaheri-src.js
+++ public/assets/js/somytaheri-src.js
@@ -1,4 +1,19 @@
-//$(function() {
+/**
+ * Created by somi on 3/12/14.
+ */
+// overlay fx on blog-slides
+$( '#blog-slides .cycle-slideshow' ).on( 'cycle-before', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+    $(outgoingSlideEl).parent().parent().siblings("#blog-slides-overlay-wrapper").find(".blog-slides-overlay").animate({
+        left: "-350px"
+    });
+});
+
+$( '#blog-slides .cycle-slideshow' ).on( 'cycle-after', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+    $(incomingSlideEl).parent().parent().siblings("#blog-slides-overlay-wrapper").find(".blog-slides-overlay").animate({
+        left: "0px"
+    });
+});
+;//$(function() {
 //    $(".page-scroll a").bind("click", function(event) {
 //        var $anchor = $(this);
 //        console.log($($anchor.attr("href")));
@@ -8,26 +23,70 @@
 //    event.preventDefault();
 //});
 //});
-//$(function() {
-//    $('a[href*=#]:not([href=#])').click(function() {
-//        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//            var target = $(this.hash);
-//            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//            if (target.length) {
-//                $('html,body').animate({
-//                    scrollTop: target.offset().top
-//                }, 1000);
-//                return false;
-//            }
-//        }
-//    });
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//$('a').click(function(){
+//    $('html, body').animate({
+//        scrollTop: $( $.attr(this, 'href') ).offset().top
+//    }, 500);
+//    return false;
 //});
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+//$(function() {
+//    $(".page-scroll a").bind("click", function(event) {
+//        var $anchor = $(this);
+//        $("html, body").stop().animate({
+//            scrollTop: $($anchor.attr("href")).offset().top
+//    }, 1500);
+//    event.preventDefault();
+//});
+//});
+
+console.log($("#home").position());
+//console.log($("#home").height());
+console.log("home");
+console.log($("#portfolio").position());
+console.log("portfolio");
+console.log($("#about").position());
+console.log($("#contact").position());
+//$("#navProfile").click(function(){
+//    console.log("hi");
+//    $("#portfolio").offset({ top: 0, left: 0});
+//    console.log($("#portfolio").position());
+//});
+//
+//$("#home").click(function() {
+//    alert( "Handler for .click() called." );
+//});
+
+//
+//var hheight=$("#home").height();
+//
+//$("#navProfile").click(function(){
+//    console.log("hi");
+//    $('html, body').animate({scrollTop : 900},800);
+//    console.log(hheight);
+//    return false;
+//});
+//
+$("a").click(function(){
+    //console.log("hi");
+    //$('html, body').animate({scrollTop : 1900},800);
+    //console.log(hheight);
+    //return false;
 });;/**
  * Created by somi on 1/12/14.
  */
