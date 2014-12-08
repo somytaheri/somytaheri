659efe2ebc5a8566c4730931e6febc760aad8afe
Somi <somy.taheri@gmail.com>
2014-12-08 17:04:20 +1100

    favicon added

--- public/assets/js/somytaheri-src.js
+++ public/assets/js/somytaheri-src.js
@@ -1,15 +1,18 @@
 var
     pageHeight = $("#home").height(),
     pageWidth = $("#home").width();
+console.log(pageWidth);
 
 var scrollAnimation = function(x,y){
-    $("#slides").transition({ x: pageWidth*-x,y:pageHeight*-y },1500,"in");
+    $("#slides").transition({ x: pageWidth*-x,y:pageHeight*-y },1000);
     return false;
 };
 
 
 $("#navHome").click(function(){
     scrollAnimation(0,0);
+    console.log("hi");
+
 });
 
 $("#navProfile").click(function(){
